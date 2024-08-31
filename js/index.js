8de0621deb9b1d6ae2af59b8530a0b7c5a356072
Rob Laughlin <waterfallkicks@gmail.com>
2024-08-31 15:14:48 -0700

    Rewrite js for index as a module

--- js/index.js
+++ js/index.js
@@ -1,36 +1,40 @@
 import TicTacToe from "./TicTacToe.js";
 import PubSub from "./PubSub.js";
 
-function render(data) {
-    const { player, square, gameState } = data;
-    const targetSquareIdx = square.row * 3 + square.col;
+const Index = (function() {
+    function render(data) {
+        const { player, square, gameState } = data;
+        const targetSquareIdx = square.row * 3 + square.col;
+        
+        const board = document.getElementById("TTTBoard");
+        let targetSquare = board.getElementsByClassName("square")[targetSquareIdx];
+        targetSquare.classList.add(player);
+        console.log(gameState);
+    };
+
+    function squareClicked(e) {
+        const squareIdx = e.target.dataset.index;
+        const row = Math.floor((squareIdx-1) / 3);
+        const col = (squareIdx-1) % 3;
+        try {
+            TicTacToe.game.playMove(row, col);
+        } catch {};
+        
+    }
+
+    function init() {
+        PubSub.subscribe("render", render);
+        TicTacToe.game.playMove(2,2);
+        TicTacToe.game.playMove(2,1);
     
-    const board = document.getElementById("TTTBoard");
-    let targetSquare = board.getElementsByClassName("square")[targetSquareIdx];
-    targetSquare.classList.add(player);
-    console.log(gameState);
-};
+        document.querySelectorAll("#TTTBoard > .square").forEach(square => {
+            square.addEventListener("click", squareClicked);
+        });
+    }
 
-function squareClicked(e) {
-    const squareIdx = e.target.dataset.index;
-    const row = Math.floor((squareIdx-1) / 3);
-    const col = (squareIdx-1) % 3;
-    try {
-        TicTacToe.game.playMove(row, col);
-    } catch {};
-    
-}
+    return { init }
+})();
 
-function init() {
-    PubSub.subscribe("render", render);
-    TicTacToe.game.playMove(2,2);
-    TicTacToe.game.playMove(2,1);
-
-    document.querySelectorAll("#TTTBoard > .square").forEach(square => {
-        square.addEventListener("click", squareClicked);
-    });
-}
-
-init();
+Index.init();
 
 
